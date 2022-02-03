88b6721237f8fae28c1da292103d9b0a76ee74aa
cyace84 <cyace84@gmail.com>
2022-02-03 04:46:31 +0200

    lowercase contract name

--- migrations/1_initial_example.js
+++ migrations/1_initial_example.js
@@ -1,7 +1,7 @@
 const { migrate } = require("../scripts/helpers");
 const exampleStorage = require("../storage/Example");
 
-module.exports = async (tezos) => {
-  const contractAddress = await migrate(tezos, "Example", exampleStorage);
+module.exports = async tezos => {
+  const contractAddress = await migrate(tezos, "example", exampleStorage);
   console.log(`Example contract address: ${contractAddress}`);
 };
